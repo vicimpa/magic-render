0e5dafe1e98201ed1712932b0fd91fbd4b1a11e9
PromiSe#### <vicimpa@ya.ru>
2024-01-12 09:08:42 +0700

    New example

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,33 @@ import { Vec2 } from "class/Vec2";
 import { fromPath } from "utils/fromPath";
 import { makeCanvas } from "utils/makeCanvas";
 import { makeLooper } from "utils/makeLooper";
-import { PI2 } from "utils/math";
+import { PI2, rem } from "utils/math";
 
-const [, ctx] = makeCanvas(document.body, .5);
+const [can, ctx] = makeCanvas(document.body, .5);
 
 const START = new Vec2();
 const ANGLE = new Vec2(0, PI2);
+const radius = 100;
+
+console.log(rem(-6, 10), -1 % 10);
 
 makeLooper(
   (_time, _dtime) => {
+    const size = Vec2
+      .fromSize(can)
+      .times(.2)
+      .plus(radius, 0);
+
     fromPath(ctx, () => {
-      ctx.arc(START, 100, ANGLE);
+      ctx.arc(
+        Vec2
+          .fromAngle(_time * .002)
+          .times(size)
+          .times(1, 1)
+        ,
+        radius,
+        ANGLE
+      );
       ctx.fill();
     });
   }
